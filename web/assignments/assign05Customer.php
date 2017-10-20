8b899f9c1ea933dab2f353b68dcd3b2043167a66
fizhy37 <fis15028@byui.edu>
2017-10-20 14:31:02 -0600

    fixing customer.php

--- web/assignments/assign05Customer.php
+++ web/assignments/assign05Customer.php
@@ -1,7 +1,6 @@
 <?php
 	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 'On');  //On or Off
-	
 	$dbUrl = getenv('DATABASE_URL');
 
 	$dbopts = parse_url($dbUrl);
@@ -30,14 +29,14 @@
 			if(isset($_GET['id'])) {
 				$statement = $db->query("SELECT * FROM customer WHERE id = '$_GET[id]'");
 				$row = $statement->fetch(PDO::FETCH_ASSOC);
-				$statement3 = $db->query("SELECT * FROM package WHERE id = $sale[packageid]");
-				$package = $statement3->fetch(PDO::FETCH_ASSOC);
 			
 				echo '<h1>' . $row['firstname'] . ' ' . $row['lastname'] . '</h1>';
 				echo '<span class="boldScrip">' . $row['firstname'] . ' ' . $row['lastname'] . '</span>  - ' . $row['address'] . '<br/><br/>';
 				
 				foreach ($db->query("SELECT * FROM sale WHERE customerid = '$_GET[id]'") as $sale)
 				{
+					$statement3 = $db->query("SELECT * FROM package WHERE id = $sale[packageid]");
+					$package = $statement3->fetch(PDO::FETCH_ASSOC);
 					echo $row['firstname'] . ' paid $' . $sale['totalcost'] . ' for the ' . $package['packagename'] . ' Package.<br/>';
 					echo 'The ' . $package['packagename'] . ' Package description is: ' . $package['description'] . '<br/>';
 				}
