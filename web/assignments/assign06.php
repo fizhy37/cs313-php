06b53d86f7b77cffb53dbcdf30c100ef046307c9
fizhy37 <fis15028@byui.edu>
2017-10-20 18:06:02 -0600

    adding assign06.php

---
+++ web/assignments/assign06.php
@@ -0,0 +1,24 @@
+<?php
+
+    include 'session_start.php';
+    include 'header.php';
+
+?>
+
+<h1>Fisher Digital Media Client Search</h1>
+	<nav>
+		<a class="bar" href="../home.html">Home</a>
+		<a class="bar" href="../assignments.html">Assignments</a>
+		<a class="bar" href="https://www.youtube.com/sabertoothzebras">YouTube</a>
+	</nav>
+	<div class="center_content">
+		<ul>
+			<a href="assign05List.php">List of Clients</a>
+			<a href="assign05Search.php">Search Clients</a>
+			<a href="">Add a Client</a>
+		</ul>
+	</div>
+
+<?php
+    include 'footer.php';
+?>
