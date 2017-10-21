fb8f515d6dbcd868b26604f6ebd270c2926feccf
fizhy37 <fis15028@byui.edu>
2017-10-20 18:03:33 -0600

    fixing login

--- web/assignments/login.php
+++ web/assignments/login.php
@@ -4,13 +4,16 @@ include 'connect_db.php';
 
 $errors = array();
 
+$stmt = $db->prepare("SELECT * FROM user_account WHERE username = ?");
+if ($stmt->execute(array($_POST['username']))) {
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $firstRow = $data[0];
+}
+
 if (isset ($_POST['username'])) {
     //pull from post and check in database
     //if correct set loggedin = true and redirect else error message
-    echo $_POST['username'];
-    echo $_POST['password'];
-    if ($db->query("SELECT username FROM customer WHERE username = '$_POST[username]'") === $_POST['username']) {
-    //if ($db->query("SELECT * FROM user WHERE username = '$_POST[username]' AND password = '$_POST[password]'"))
+    if ($_POST['password'] === $firstRow['password']) {
         $_SESSION['loggedin'] = true;
         header('Location: assign06.php');
     } else {
