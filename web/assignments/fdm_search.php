34bb4e7799aad9a073e76ff5317d2373fa0925b6
fizhy37 <fis15028@byui.edu>
2017-10-21 15:54:21 -0600

    fixing search styling

--- web/assignments/fdm_search.php
+++ web/assignments/fdm_search.php
@@ -4,26 +4,34 @@
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Database Search</title>
-		<script src="fdm_js.js"></script>
-	</head>
-	<body>
-		<div class="middle_col">
-		<h1>Search Clients</h1>
-		<form action="" method="POST">
-			<label for="customer">Search Clients by First or Last Name: </label><input type="text" name="customer"/>
-			<input type="submit" value="Search"/>
-		</form>
-		<?php
-			if(isset($_POST['customer'])) {
-				foreach ($db->query("SELECT * FROM customer WHERE firstname = '$_POST[customer]' OR lastname = '$_POST[customer]' OR address = '$_POST[customer]'") as $row)
-				{
-				  echo '<a href="fdm_customer.php?id=' . $row['id'] . '">' . $row['firstname'] . ' ' . $row['lastname'] . '</a>';
-				  echo '<br/>';
+<head>
+	<title>Database Search</title>
+	<script src="fdm_js.js"></script>
+</head>
+
+<body>
+	<nav>
+        <a class="bar" href="../home.html">Home</a>
+        <a class="bar" href="../assignments.html">Assignments</a>
+        <a class="bar" href="http://fisherdigitalmedia.businesscatalyst.com">FDM Website</a>
+        <a class="bar" href="https://www.youtube.com/sabertoothzebras">YouTube</a>
+    </nav>
+    <div class="topper">
+        <h1>Search Clients</h1>
+        <div class="nav_list">
+			<form action="" method="POST">
+				<label for="customer">Search Clients by First or Last Name: </label><input type="text" name="customer"/>
+				<input type="submit" value="Search"/>
+			</form>
+			<?php
+				if(isset($_POST['customer'])) {
+					foreach ($db->query("SELECT * FROM customer WHERE firstname = '$_POST[customer]' OR lastname = '$_POST[customer]' OR address = '$_POST[customer]'") as $row)
+					{
+						echo '<button class="client_buttons" onclick="open_page(\'fdm_customer.php?id=' . $row['id'] . '\')">' . $row['firstname'] . ' ' . $row['lastname'] . '</button>';
+						echo '<br/>';
+					}
 				}
-			}
-		?>
-		</div>
-	</body>
+			?>
+	</div>
+</body>
 </html>
