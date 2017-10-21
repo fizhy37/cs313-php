011f7864aed732af4b48be99468254c559b7a8cc
fizhy37 <fis15028@byui.edu>
2017-10-21 15:06:06 -0600

    added blue color scheme

--- web/assignments/styling.php
+++ web/assignments/styling.php
@@ -19,12 +19,13 @@
             <a class="bar" href="https://www.youtube.com/sabertoothzebras">YouTube</a>
         </nav>
         <div class="topper">
-            <h1>Styling Testing</h1>
-        </div>
-        <div class="nav_list">
-            <button id="fdmlist" class="client_buttons" onclick="open_page('fdm_list.php')">List of Clients</button>
-            <button id="fdmsearch" class="client_buttons" onclick="open_page('fdm_search.php')">Search Clients</button>
-            <button id="fdmadd" class="client_buttons" onclick="open_page('fdm_add_client.php')">Add Client</button>
+            <h1>Fisher Digital Media Testing</h1>
+            <div class="nav_list">
+                <button id="login" class="client_buttons" onclick="open_page('login.php')">Login</button>
+                <button id="fdmlist" class="client_buttons" onclick="open_page('fdm_list.php')">List of Clients</button>
+                <button id="fdmsearch" class="client_buttons" onclick="open_page('fdm_search.php')">Search Clients</button>
+                <button id="fdmadd" class="client_buttons" onclick="open_page('fdm_add_client.php')">Add Client</button>
+            </div>
         </div>
 	</body>
 </html>
