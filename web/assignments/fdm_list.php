29ddd77d7a9e1062c4d57adafa20f86a7f363333
fizhy37 <fis15028@byui.edu>
2017-10-21 15:43:20 -0600

    styling fdmlist

--- web/assignments/fdm_list.php
+++ web/assignments/fdm_list.php
@@ -5,27 +5,38 @@
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Client List</title>
-		<link rel="stylesheet" href="db.css">
-		<style>
-			.boldScrip {
-				font-weight: bold;
-			}
-		</style>
-		<script src="fdm_js.js"></script>
-	</head>
-	<body>
-		<div class="middle_col">
-		<h1>Select a Client</h1>
+<head>
+	<title>Client List</title>
+	<link rel="stylesheet" href="db.css">
+	<style>
+		.boldScrip {
+			font-weight: bold;
+		}
+	</style>
+	<script src="fdm_js.js"></script>
+</head>
+<body>
+	<nav>
+        <a class="bar" href="../home.html">Home</a>
+        <a class="bar" href="../assignments.html">Assignments</a>
+        <a class="bar" href="http://fisherdigitalmedia.businesscatalyst.com">FDM Website</a>
+        <a class="bar" href="https://www.youtube.com/sabertoothzebras">YouTube</a>
+    </nav>
+    <div class="topper">
+        <h1>Select a Client</h1>
+        <div class="nav_list">
 		<?php
 			foreach ($db->query("SELECT * FROM customer") as $row)
 			{
-                echo '<a href="fdm_customer.php?id=' . $row['id'] . '">' . $row['firstname'] . ' ' . $row['lastname'] . '</a>';
-                echo '<br/>';
+				echo '<button class="client_buttons" onclick="open_page(\'fdm_customer.php?id=' . $row['id'] . '\')">' . $row['firstname'] . ' ' . $row['lastname'] . '</button>';
+				echo '<br/>';
 			}
 		?>
-		<br/>
-		</div>
-	</body>
+            <button id="login" class="client_buttons" onclick="open_page('login.php')">Login</button>
+            <button id="fdmlist" class="client_buttons" onclick="open_page('fdm_list.php')">List of Clients</button>
+            <button id="fdmsearch" class="client_buttons" onclick="open_page('fdm_search.php')">Search Clients</button>
+            <button id="fdmadd" class="client_buttons" onclick="open_page('fdm_add_client.php')">Add Client</button>
+        </div>
+	</div>
+</body>
 </html>
