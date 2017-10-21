0cf5667401dae7a6b6aee1af1bb8dd35394b0c18
fizhy37 <fis15028@byui.edu>
2017-10-21 15:11:23 -0600

    add blue color scheme

--- web/assignments/assign06.php
+++ web/assignments/assign06.php
@@ -16,15 +16,16 @@
 </head>
 
 <body>
-	<nav>
-		<a class="bar" href="../home.html">Home</a>
+    <nav>
+        <a class="bar" href="../home.html">Home</a>
         <a class="bar" href="../assignments.html">Assignments</a>
         <a class="bar" href="http://fisherdigitalmedia.businesscatalyst.com">FDM Website</a>
-		<a class="bar" href="https://www.youtube.com/sabertoothzebras">YouTube</a>
-	</nav>
-	<div class="topper">
-    <h1>Fisher Digital Media - Clients</h1>
+        <a class="bar" href="https://www.youtube.com/sabertoothzebras">YouTube</a>
+    </nav>
+    <div class="topper">
+        <h1>Fisher Digital Media - Client</h1>
         <div class="nav_list">
+            <button id="login" class="client_buttons" onclick="open_page('login.php')">Login</button>
             <button id="fdmlist" class="client_buttons" onclick="open_page('fdm_list.php')">List of Clients</button>
             <button id="fdmsearch" class="client_buttons" onclick="open_page('fdm_search.php')">Search Clients</button>
             <button id="fdmadd" class="client_buttons" onclick="open_page('fdm_add_client.php')">Add Client</button>
