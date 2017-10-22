e13f9d7e6d5d64f0ce8cb2045e00b5811ecff399
fizhy37 <fis15028@byui.edu>
2017-10-21 20:07:27 -0600

    altering logout

--- web/assignments/logout.php
+++ web/assignments/logout.php
@@ -1,14 +1,10 @@
 <?php
 
-if (isset ($_SESSION)) {
+
     session_unset();  
     session_destroy();
     header('Location: login.php');
     exit;
-} else {
-    header('Location: assign06.php');
-    exit;
-}
 
 
 ?>
