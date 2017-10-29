a5ad85a9893d9bc83c6c7c1dbbcb084cd9b89f31
fizhy37 <fis15028@byui.edu>
2017-10-28 23:35:02 -0600

    fixing signup

--- web/assignments/session_start.php
+++ web/assignments/session_start.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if (!isset ($_SESSION['loggedin']) || !$_SESSION['loggedin']) { 
+if (!isset ($_SESSION['username'])) { 
     header('Location: login.php');
     die();
     exit;
