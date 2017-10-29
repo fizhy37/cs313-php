1a551ba47546173c9b7332789cec0c3329a8df47
fizhy37 <fis15028@byui.edu>
2017-10-28 21:51:54 -0600

    adding signup

--- web/assignments/session_start.php
+++ web/assignments/session_start.php
@@ -3,9 +3,9 @@
 session_start();
 
 if (!isset ($_SESSION['loggedin']) || !$_SESSION['loggedin']) { 
-    header('Location: login.php');
-    die();
-    exit;
+    //header('Location: login.php');
+    //die();
+    //exit;
 }
 
 $dbUrl = getenv('DATABASE_URL');
